10c2a06ef4d4a4799f11f5f474d5942bead579be
Nkeze Sylvester <snkeze9@gmail.com>
2022-04-11 12:30:09 +0100

    Maintenance mode updated

--- resources/js/pages/Error.vue
+++ resources/js/pages/Error.vue
@@ -1,34 +1,53 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <div>{{ description }}</div>
-    <div>{{previous_url}}</div>
-  </div>
+    <div class="border-4 min-h-screen px-4 bg-gray-100 border-primary-500">
+        <div
+            class="max-w-xl my-5 mt-20 py-8 px-5 bg-white rounded-md shadow-lg mx-auto"
+        >
+            <a
+                href="/"
+                class="inline-flex mx-auto items-center mb-5 w-auto rounded-md border border-primaryBg-400 p-2"
+            >
+                <img class="w-10" :src="$page.auth.settings.auth_logo" alt="" />
+                <p class="ml-3 text-black font-bold capitalize">
+                    {{ $page.auth.settings.app_name }}
+                </p>
+            </a>
+            <h1 class="text-2xl md:text-4xl font-bold mb-4">{{ title }}</h1>
+            <div class="text-sm mb-6">{{ description }}</div>
+            <div>
+                <a
+                    :href="previous_url"
+                    class="py-2 px-4 rounded-lg text-black bg-yellow-500 hover:bg-yellow-700 focus:bg-yellow-700"
+                    >Go back to previous page</a
+                >
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  props: {
-    status: Number,
-    previous_url: String,
-  },
-  computed: {
-    title() {
-      return {
-        503: '503: Service Unavailable',
-        500: '500: Server Error',
-        404: '404: Page Not Found',
-        403: '403: Forbidden',
-      }[this.status]
+    props: {
+        status: Number,
+        previous_url: String,
     },
-    description() {
-      return {
-        503: 'Sorry, we are doing some maintenance. Please check back soon.',
-        500: 'Whoops, something went wrong on our servers.',
-        404: 'Sorry, the page you are looking for could not be found.',
-        403: 'Sorry, you are forbidden from accessing this page.',
-      }[this.status]
+    computed: {
+        title() {
+            return {
+                503: "Website on maintenance mode",
+                500: "Server Error",
+                404: "Page Not Found",
+                403: "Forbidden",
+            }[this.status];
+        },
+        description() {
+            return {
+                503: "Sorry, we are doing some maintenance. Please check back soon.",
+                500: "Whoops, something went wrong on our servers.",
+                404: "Sorry, the page you are looking for could not be found.",
+                403: "Sorry, you are forbidden from accessing this page.",
+            }[this.status];
+        },
     },
-  },
-}
+};
 </script>
